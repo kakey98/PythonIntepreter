e118297406ac5db1a68db282cfe8b3d3746d529d
kakey98 <kakey98@gmail.com>
2022-03-06 10:03:37 +0900

    Add class Intepreter
    Add

--- Intepreter.py
+++ Intepreter.py
@@ -12,3 +12,56 @@ class Token(object):
         return self.__str__()
 
     
+class Intepreter(object):
+    def __init__(self, text):
+        self.text = text
+        self.pos = 0
+        self.current_token = None
+
+    def error(self):
+        raise Exception('Error parsing input')
+
+    def get_next_token(self):
+        text = self.text
+
+        if self.pos > len(self.text) - 1:
+            return Token(EOF, None)
+
+        current_char = text[self.pos]
+
+        if current_char.isdigit():
+            token = Token(INTEGER, int(current_char))
+            self.pos += 1
+            return token
+
+        
+        if current_char == '+':
+            token = Token(PLUS, current_char)
+            self.pos += 1
+            return token
+        
+        self.error
+
+    
+    def eat(self, token_type):
+        if self.current_token.type == token_type:
+            self.current_token = self.get_next_token()
+        else:
+            self.error()
+
+    def expr(self):
+        self.current_token = self.get_next_token
+
+        left = self.current_token
+        self.eat(INTEGER)
+
+        op = self.current_token
+        self.eat(PLUS)
+
+        right = self.current_token
+        self.eat(INTEGER)
+
+        result = left.value + right.value
+
+        return result
+        
